be1bf9620f6f6f8ac5350750f15b71a7fbd083e2
Cris Lom <36197748+Lombardoc4@users.noreply.github.com>
2024-01-08 10:48:12 -0500

    Revert "chore: build out landing page"
    This reverts commit 5cc02101c46fafe9bf2bf408c0143de911be40f8.

--- src/app/lib/utils.tsx
+++ src/app/lib/utils.tsx
@@ -1,5 +1,3 @@
-import { faker } from '@faker-js/faker';
-
 /**
  * @description
  * Get all possible seasons/years with results
@@ -8,20 +6,20 @@ import { faker } from '@faker-js/faker';
 export const f1Seasons = (): string[] => {
   // Discuss : Bump to fetch call to get data
   // ! Alter to be dynamic
-  // const testingDate = new Date('02/22/2024');
+  const testingDate = new Date('02/22/2024');
 
   const currDate = new Date();
-  const currYear = currDate.getFullYear();
+  let currYear = currDate.getFullYear();
 
   // Compare curr date to testing date (Feb 22 2024)
   // If same year as testing and before testing date
   // Get previous year
-  // if (
-  //   testingDate.getFullYear() === currYear &&
-  //   currDate.getTime() < testingDate.getTime()
-  // ) {
-  //   currYear -= 1;
-  // }
+  if (
+    testingDate.getFullYear() === currYear &&
+    currDate.getTime() < testingDate.getTime()
+  ) {
+    currYear -= 1;
+  }
 
   // Fill array with values between range
   return Array.from({ length: currYear - 1950 + 1 }, (_v, index) =>
@@ -29,66 +27,9 @@ export const f1Seasons = (): string[] => {
   );
 };
 
-export interface ISchedule {
-  RoundNumber: number;
-  Country: string;
-  Location: string;
-  OfficialEventName: string;
-  EventDate: string;
-  EventName: string;
-  EventFormat: string;
-  Session1: string;
-  Session1Date: string;
-  Session1DateUtc: string;
-  Session2: string;
-  Session2Date: string;
-  Session2DateUtc: string;
-  Session3: string;
-  Session3Date: string;
-  Session3DateUtc: string;
-  Session4: string;
-  Session4Date: string;
-  Session4DateUtc: string;
-  Session5: string;
-  Session5Date: string;
-  Session5DateUtc: string;
-  F1ApiSupport: boolean;
-}
-
-interface IDataConfigs {
-  seasons: string[];
-  schedule: ISchedule[];
-  drivers: string[];
-  sessions: string[];
-}
-
-const dataConfig: IDataConfigs = {
+const dataConfig: { [key: string]: string[] } = {
   seasons: f1Seasons(),
-  schedule: Array.from(Array(3).keys()).map(() => ({
-    RoundNumber: 0,
-    Country: faker.location.country(),
-    Location: faker.location.city(),
-    OfficialEventName: faker.word.words(5),
-    EventDate: faker.date.future({ years: 1 }).toString(),
-    EventName: faker.word.words(3),
-    EventFormat: 'string',
-    Session1: 'string',
-    Session1Date: faker.date.future({ years: 1 }).toString(),
-    Session1DateUtc: faker.date.future({ years: 1 }).toString(),
-    Session2: 'string',
-    Session2Date: faker.date.future({ years: 1 }).toString(),
-    Session2DateUtc: faker.date.future({ years: 1 }).toString(),
-    Session3: 'string',
-    Session3Date: faker.date.future({ years: 1 }).toString(),
-    Session3DateUtc: faker.date.future({ years: 1 }).toString(),
-    Session4: 'string',
-    Session4Date: faker.date.future({ years: 1 }).toString(),
-    Session4DateUtc: faker.date.future({ years: 1 }).toString(),
-    Session5: 'string',
-    Session5Date: faker.date.future({ years: 1 }).toString(),
-    Session5DateUtc: faker.date.future({ years: 1 }).toString(),
-    F1ApiSupport: true,
-  })),
+  races: ['All Races', 'Bahrain', 'Mexico', 'Monaco', 'Imola', 'Spain'],
   drivers: [
     'All Drivers',
     'Drive 1',
@@ -97,43 +38,32 @@ const dataConfig: IDataConfigs = {
     'Drive 4',
     'Drive 5',
   ],
-  sessions: ['Practice 1', 'Practice 2', 'Practice 3', 'Qualifying', 'Race'],
 };
 
-export const fetchAPI = async (endpoint: string) => {
+export const fetchAPI = (endpoint: string) => {
   const server = document.body.classList.contains('server');
-  const dummy: string[] | ISchedule[] | false =
-    dataConfig[
-      endpoint.split('/')[0] as 'seasons' | 'schedule' | 'drivers' | 'sessions'
-    ] || false;
+  const dummy: string[] | false = dataConfig[endpoint] || false;
 
   if (!server) {
     return dummy;
   } else {
     // Fetch from server
     // TODO : update to axios
-    const data = await fetch(`http://0.0.0.0:80/${endpoint}`)
-      .then(
-        (res) => {
-          if (!res.ok) {
-            return dummy;
-          }
+    const data = fetch(`http://0.0.0.0:80/${endpoint}`).then(
+      (res) => {
+        if (!res.ok) {
+          // console.error(`Backend responded with ${res.status} error`);
+          return null;
+        }
+        return res.json();
+      },
+      () => {
+        // console.error('Could not reach backend', error);
+        return null;
+      },
+    );
+    // if error use dummy data
 
-          return res.json();
-        },
-        () => {
-          // console.log('server error');
-
-          return dummy;
-        },
-      )
-      .then((data) => data)
-      .catch(() => {
-        // console.log('catch');
-        return dummy;
-      });
-
-    // console.log('data', data);
-    return data;
+    return data || dataConfig;
   }
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,19 @@
-import { MainFilters } from './MainFilters';
-
 export default function Home() {
   return (
     <main>
-      <Hero />
-
-      {/* Suspense */}
-      <NextRace />
+      <div className='hero min-h-screen bg-base-200'>
+        <div className='hero-content text-center'>
+          <div className='max-w-md'>
+            <h1 className='text-5xl font-bold'>Hello</h1>
+            <p className='py-6'>
+              We are Slick Telemetry, like-minded individuals and fans of
+              Formula 1. We are currently building an analysis platform for F1
+              Data Analysis!
+            </p>
+            <button className='btn btn-primary'>Check it out</button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
-
-const Hero = () => (
-  <div className='hero my-16 lg:my-20'>
-    <div className='hero-content gap-8  lg:w-4/5 lg:justify-start'>
-      <div className='flex flex-col gap-8 '>
-        <h1 className='text-5xl font-bold lg:text-7xl'>Slick Telemetry</h1>
-        <MainFilters />
-        <p className='max-w-sm text-sm lg:max-w-md lg:text-base'>
-          We are Slick Telemetry, like-minded individuals and fans of Formula 1.
-          We are currently building an analysis platform for F1 Data Analysis!
-        </p>
-      </div>
-    </div>
-  </div>
-);
-
-const NextRace = () => {
-  return (
-    <div className='bg-base-300 px-4 py-8'>
-      <h2 className='text-2xl'>Winter Testing</h2>
-      Bahrain Feb 21, 2024
-    </div>
-  );
-};
