39f317c8a9abe2f8b436daedc6fc12373e27526e
cris lombardo <bigc4997@gmail.com>
2024-01-08 10:37:20 -0500

    chore: include main filters and available server data for results ui

--- src/app/results/layout.tsx
+++ src/app/results/layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { f1Seasons } from '../lib/utils';
-import { Dropdown } from '../ui/Dropdown';
+import { MainFilters } from '../MainFilters';
 
 // Default Next Layout
 export default function ResultsLayout({
@@ -11,41 +10,10 @@ export default function ResultsLayout({
 }) {
   return (
     <>
-      <ResultsNav />
+      <div className='container mx-auto mb-4 rounded-box bg-base-300 p-4 lg:my-4'>
+        <MainFilters />
+      </div>
       {children}
     </>
   );
 }
-
-const seasons = f1Seasons();
-const dummyRaces = [
-  'All Races',
-  'Bahrain',
-  'Mexico',
-  'Monaco',
-  'Imola',
-  'Spain',
-];
-const dummyDrivers = [
-  'All Drivers',
-  'Drive 1',
-  'Drive 2',
-  'Drive 3',
-  'Drive 4',
-  'Drive 5',
-];
-
-const ResultsNav = () => {
-  return (
-    <div className='container navbar mx-auto my-4 flex-col items-start rounded-box bg-base-300 lg:flex-row lg:items-center'>
-      <h2 className='p-2 text-lg font-bold'>Results:</h2>
-      <Dropdown value={seasons[0]} items={seasons} action={() => {}} />
-      <Dropdown value={dummyRaces[0]} items={dummyRaces} action={() => {}} />
-      <Dropdown
-        value={dummyDrivers[0]}
-        items={dummyDrivers}
-        action={() => {}}
-      />
-    </div>
-  );
-};
