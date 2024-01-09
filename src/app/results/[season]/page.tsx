0e089216bc9230a604d6e1605ca3f7f0c8718502
cris lombardo <bigc4997@gmail.com>
2024-01-09 16:17:52 -0500

    feat: url updates when results change

--- src/app/results/[season]/page.tsx
+++ src/app/results/[season]/page.tsx
@@ -5,9 +5,9 @@ import { useAtom } from 'jotai';
 import ResultsPage from '../SeasonResults';
 import { handleSeasonChangeAtom } from '../../../atoms/results';
 
-export default function Page({ params }: { params: { slug: string } }) {
+export default function Page({ params }: { params: { season: string } }) {
   const [, handleSeasonChange] = useAtom(handleSeasonChangeAtom);
-  handleSeasonChange(params.slug);
+  handleSeasonChange(params.season);
 
   return <ResultsPage />;
 }
