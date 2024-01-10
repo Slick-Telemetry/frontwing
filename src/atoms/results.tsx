1d68f60d0f8a5cd308ff8ba9b4f93acb4ef07cc4
cris lombardo <bigc4997@gmail.com>
2024-01-10 13:30:10 -0500

    refactor: utilize axios for fetch calls and include status checks to see if server is active

--- src/atoms/results.tsx
+++ src/atoms/results.tsx
@@ -8,11 +8,10 @@ import {
   ISchedule,
 } from '../app/lib/utils';
 
-//
 export const raceAtom = atom('All Races');
 export const seasonRacesAtom = atom<ISchedule[]>([]);
 export const seasonAtom = atom('2023');
-export const seasonsAtom = atom<string[]>([]);
+export const allSeasonsAtom = atom<string[]>([]);
 export const driverAtom = atom('All Drivers');
 export const driversAtom = atom<string[]>([]);
 export const sessionAtom = atom('Race');
@@ -22,6 +21,9 @@ export const resultUrlAtom = atom('/results');
 export const constructorStandingsAtom = atom<IConstructorStandings[]>([]);
 
 // Derived Atoms
+
+// Dropdown values for races
+// Returns string[] of race event names
 export const raceNamesDropdownAtom = atom((get) => {
   const data = get(seasonRacesAtom).map((race) => race.EventName);
   // Add All Races option
@@ -30,27 +32,36 @@ export const raceNamesDropdownAtom = atom((get) => {
 });
 
 // Effect Atoms
+// Get Seasons values, this is done clientside
+// Dependencies: seasonsAtom
 export const fetchSeasons = atomEffect((get, set) => {
-  if (get(seasonsAtom).length <= 0) {
-    fetchAPI('seasons').then((data) => set(seasonsAtom, data));
+  // Seasons do not change, only fetch if empty array
+  if (get(allSeasonsAtom).length <= 0) {
+    fetchAPI('seasons').then((data) => set(allSeasonsAtom, data));
   }
 });
 
+// Get Races per year
+// Dependencies: seasonAtom
 export const fetchRaces = atomEffect((get, set) => {
-  fetchAPI('schedule/' + get(seasonAtom)).then((data) => {
+  fetchAPI('schedule?year=' + get(seasonAtom)).then((data) => {
     set(seasonRacesAtom, data);
   });
 });
 
+// Get Driver per ...season & race
 export const fetchDriver = atomEffect((get, set) => {
   fetchAPI('drivers').then((data) => set(driversAtom, data));
 });
+// Get sessions per ...season & race
 export const fetchSessions = atomEffect((get, set) => {
   fetchAPI('sessions').then((data) => set(sessionsAtom, data));
 });
 
+// Get Driver & Constructor Standings
 export const fetchStandings = atomEffect((get, set) => {
   fetchAPI('standings').then((data) => {
+    // Flatten constructor values
     const constructors = data.constructors.map(
       (con: IConstructorStandingsFetch) => ({
         pos: con.position,
@@ -72,6 +83,9 @@ export const toggleTelemetryDisableAtom = atomEffect((get, set) => {
 });
 
 // Write only Atoms
+// aka Update other atoms
+
+// Update Values when a season changes
 export const handleSeasonChangeAtom = atom(
   null,
   async (get, set, update: string) => {
@@ -85,6 +99,7 @@ export const handleSeasonChangeAtom = atom(
   },
 );
 
+// Update Values when a Race changes
 export const handleRaceChangeAtom = atom(
   null,
   async (get, set, update: string) => {
@@ -96,11 +111,12 @@ export const handleRaceChangeAtom = atom(
   },
 );
 
+// Update Values when a Driver changes
 export const handleDriverChangeAtom = atom(
   null,
   async (get, set, update: string) => {
     set(driverAtom, update);
-    set(sessionAtom, 'Race');
+    // set(sessionAtom, 'Race');
     set(
       resultUrlAtom,
       '/results/' + get(seasonAtom) + '/' + get(raceAtom) + '/' + update,
@@ -110,6 +126,7 @@ export const handleDriverChangeAtom = atom(
   },
 );
 
+// Update Values when a session changes
 export const handleSessionChangeAtom = atom(
   null,
   async (get, set, update: string) => {
@@ -130,6 +147,7 @@ export const handleSessionChangeAtom = atom(
   },
 );
 
+// Handle click of results button in <MainFilters/>
 export const handleResultsAtom = atom(null, (get, set) => {
   set(seasonRacesAtom, get(seasonRacesAtom));
 });
