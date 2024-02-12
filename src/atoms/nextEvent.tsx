fa4b8c849ca366afed9357117e90047bb9e24869
Cris Lom <36197748+Lombardoc4@users.noreply.github.com>
2024-02-12 11:16:01 -0500

    feat: FRON-11 showcase next upcoming event (#26)

---
+++ src/atoms/nextEvent.tsx
@@ -0,0 +1,15 @@
+import { atom } from 'jotai';
+import { atomEffect } from 'jotai-effect';
+
+// Next Event
+export const nextEventLiveAtom = atom(false);
+export const nextEventAtom = atom<NextEventProps | null>(null);
+export const nextEventTimeAtom = atom(0);
+export const nextEventEffect = atomEffect((get, set) => {
+  if (get(nextEventTimeAtom) !== 0) {
+    const intervalId = setInterval(() => {
+      set(nextEventTimeAtom, (prev: number) => prev - 1000);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }
+});
