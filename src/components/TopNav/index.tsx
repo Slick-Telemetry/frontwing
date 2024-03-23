fd6eaefd0a24b6dc09c739546e62c62c52d25738
cris lombardo <bigc4997@gmail.com>
2024-03-22 21:05:31 -0400

    chore: sideline user profile for next version

--- src/components/TopNav/index.tsx
+++ src/components/TopNav/index.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { UserNav } from '.';
 import { MainNav } from './MainNav';
-import Logo from './slick-telemetry-logo.webp';
 
 export * from './MainNav';
 export * from './UserNav';
@@ -15,13 +13,20 @@ export const TopNav = () => {
         href='/'
         className='btn btn-ghost flex items-center gap-x-2 text-xl font-extrabold tracking-tight '
       >
-        <Image src={Logo} width={24} height={24} alt='Slick Telemetry Logo' />
+        <Image
+          src='/slick-telemetry-logo.png'
+          width={24}
+          height={24}
+          alt='Slick Telemetry Logo'
+        />
         Slick Telemetry
       </Link>
       <MainNav />
-      <div className='ml-auto flex items-center space-x-4'>
+      {/*
+      Sidelined for v2
+      <div className='flex items-center ml-auto space-x-4'>
         <UserNav />
-      </div>
+      </div> */}
     </div>
   );
 };
