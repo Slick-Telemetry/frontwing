455f43fa169a32eb49a5037f4f51546729f6367a
cris lombardo <bigc4997@gmail.com>
2024-03-31 21:40:59 -0400

    feat: FRON-37 collapsible sidebar

--- src/components/Sidebar/SelectionGroup.tsx
+++ src/components/Sidebar/SelectionGroup.tsx
@@ -19,14 +19,14 @@ export const SelectionGroup = (props: {
       defaultValue={!props.disabled ? [props.title] : []}
     >
       <AccordionItem value={props.title}>
-        <AccordionTrigger disabled={props.disabled} className='py-0'>
-          <Selection {...props} />
+        <AccordionTrigger disabled={props.disabled} className='gap-4 py-0'>
+          <Selection {...props} className='w-full' />
         </AccordionTrigger>
         <AccordionContent>
           <div className='flex flex-col'>
-            <Selection title='Query 1' className='pl-8' />
-            <Selection title='Query 2' className='pl-8' />
-            <Selection title='Query 3' className='pl-8' />
+            <Selection title='Query 1' />
+            <Selection title='Query 2' />
+            <Selection title='Query 3' />
           </div>
         </AccordionContent>
       </AccordionItem>
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,9 @@
 'use client';
 
-import { useAtom } from 'jotai';
+import { atom, useAtom } from 'jotai';
+import { FaChevronLeft } from 'react-icons/fa';
+
+import { cn } from '@/lib/utils';
 
 import {
   DriverState,
@@ -10,24 +13,46 @@ import {
 } from '@/state-mgmt/atoms';
 
 import { SelectionGroup } from './SelectionGroup';
+import { Button } from '../ui/button';
 
 export * from './Selection';
 export * from './SelectionGroup';
 
+const sidebarOpenAtom = atom(false);
 export const Sidebar = () => {
   const [season] = useAtom(SeasonState);
   const [event] = useAtom(EventState);
   const [session] = useAtom(SessionState);
   const [driver] = useAtom(DriverState);
+  const [sidebarOpen, setSidebarOpen] = useAtom(sidebarOpenAtom);
 
   // Use state values to define which groups are active
 
   return (
-    <aside className='grid gap-1 px-2'>
-      <SelectionGroup title='Season' disabled={!season} />
-      <SelectionGroup title='Event' disabled={!event} />
-      <SelectionGroup title='Drivers' disabled={!session} />
-      <SelectionGroup title='Laps' disabled={!driver} />
+    <aside
+      className={cn('relative w-48 border-r pr-6 duration-300', {
+        'w-0': sidebarOpen,
+      })}
+    >
+      <div className='grid gap-2 overflow-hidden'>
+        <SelectionGroup title='Season' disabled={!season} />
+        <SelectionGroup title='Event' disabled={!event} />
+        <SelectionGroup title='Drivers' disabled={!session} />
+        <SelectionGroup title='Laps' disabled={!driver} />
+      </div>
+      <Button
+        variant='outline'
+        size='icon'
+        className='absolute right-0 top-0 h-6 w-6 translate-x-2/4 rounded-full'
+        onClick={() => setSidebarOpen(!sidebarOpen)}
+      >
+        <FaChevronLeft
+          className={cn('h-2 w-2 transition-transform duration-200', {
+            '-rotate-180': sidebarOpen,
+          })}
+        />
+        <span className='sr-only'>Collapse</span>
+      </Button>
     </aside>
   );
 };
