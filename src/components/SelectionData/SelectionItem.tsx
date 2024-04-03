a3d63abbcb21c1bd7232bcd2d509ca72db625b5a
Cris Lom <36197748+Lombardoc4@users.noreply.github.com>
2024-04-02 20:20:24 -0400

    chore: FRON 38 initial selection lists for season and event (#43)

---
+++ src/components/SelectionData/SelectionItem.tsx
@@ -0,0 +1,17 @@
+export const SelectionItem = ({
+  children,
+  clickHandler,
+}: {
+  children: React.ReactNode | React.ReactNode[];
+  clickHandler: () => void;
+}) => {
+  return (
+    <a
+      onClick={clickHandler}
+      tabIndex={0}
+      className='cursor-pointer border-b bg-card px-4 py-2 text-card-foreground hover:bg-accent hover:text-accent-foreground'
+    >
+      {children}
+    </a>
+  );
+};
