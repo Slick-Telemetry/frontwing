319c5d3113649f03bc1f9fbbff8f60d535e68515
cris lombardo <bigc4997@gmail.com>
2025-02-18 21:13:05 -0500

    chore: format chart data in one swoop

--- src/app/standings/drivers.tsx
+++ src/app/standings/drivers.tsx
@@ -9,13 +9,11 @@ import {
   Legend,
   Line,
   LineChart,
-  LineProps,
   ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
 } from 'recharts';
-import { Payload } from 'recharts/types/component/DefaultLegendContent';
 
 import { GET_DRIVER_STANDINGS } from '@/lib/queries';
 
@@ -25,127 +23,76 @@ import {
 } from '@/generated/types';
 
 const season = 2024;
-
-const lineTypes = ['solid', '3 3', '5 5', '10 5', '10 10'];
-
-const getLineType = (index: number) => {
-  return lineTypes[index % lineTypes.length];
-};
-
-const colorMap = new Map<string, number>();
-const defaultOptions = {
-  legendType: 'circle' as const,
-  type: 'monotone' as const,
-};
-const getLineAttributes = (
-  driver: string | null | undefined,
-  standings: GetDriverStandingsQuery['driver_standings'],
-): Omit<LineProps, 'ref'> => {
-  const driverOptions = {
-    ...defaultOptions,
-    key: driver,
-    dataKey: driver || 'Unknown Driver',
-    type: 'monotone' as const,
-  };
-
-  const color = standings.find(
-    (standing) => standing.driver?.abbreviation === driver,
-  )?.driver?.driver_sessions[0].constructorByConstructorId?.color;
-
-  if (!color) return driverOptions;
-
-  if (!colorMap.has(color)) {
-    colorMap.set(color, 0);
-  } else {
-    colorMap.set(color, colorMap.get(color)! + 1);
-  }
-
-  const lineType = getLineType(colorMap.get(color)!);
-
-  return {
-    ...driverOptions,
-    stroke: `#${color}`,
-    strokeDasharray: lineType,
-  };
-};
+const strokeDashArray = ['solid', '3 3', '5 5'];
 
 interface CustomLegendProps {
-  payload?: Payload[];
   onClick: (value: string) => void;
-  driverStandings: GetDriverStandingsQuery['driver_standings'];
+  hiddenDrivers: string[];
+  constructors: {
+    name: string;
+    color?: string | null;
+    drivers: string[];
+  }[];
 }
 const CustomLegend = ({
-  payload,
   onClick,
-  driverStandings,
+  hiddenDrivers,
+  constructors,
 }: CustomLegendProps) => {
-  const groupedDrivers = payload?.reduce(
-    (acc, entry) => {
-      const driverAbbreviation = entry.value;
-      const constructor = driverStandings.find(
-        (standing) => standing.driver?.abbreviation === driverAbbreviation,
-      )?.driver?.driver_sessions[0].constructorByConstructorId?.name;
-
-      if (!constructor) return acc;
-      if (!acc[constructor]) acc[constructor] = [];
-      acc[constructor].push(entry);
-      return acc;
-    },
-    {} as Record<string, Payload[]>,
-  );
+  const handleConstructorClick = (
+    constructor: CustomLegendProps['constructors'][number],
+  ) => {
+    constructor.drivers.map((driver) => {
+      // TODO: handle situation where driver has already been clicked
+      // Make both drivers same state
+      onClick(driver);
+    });
+  };
 
   return (
     <div className='grid grid-cols-10'>
-      {groupedDrivers &&
-        Object.entries(groupedDrivers).map(([constructor, drivers], index) => (
+      {constructors.map((c) => (
+        <div
+          key={c.name}
+          className='flex flex-col text-center'
+          style={{ color: `#${c.color}` }}
+        >
           <div
-            key={constructor + index}
-            className='flex flex-col text-center'
-            style={{ color: `#${constructor}` }}
+            className='line-clamp-1 text-xs capitalize'
+            onClick={() => handleConstructorClick(c)}
           >
-            <div className='line-clamp-1 text-xs capitalize'>
-              {constructor.replace(/_/g, ' ')}
-            </div>
-            {drivers.map((driver, idx) => (
-              <div key={idx} onClick={() => onClick(driver.value)}>
-                {driver.value}
-              </div>
-            ))}
+            {c.name.replace(/_/g, ' ')}
           </div>
-        ))}
+          {c.drivers.map((driver, idx) => (
+            <div
+              key={idx}
+              onClick={() => onClick(driver)}
+              className={hiddenDrivers.includes(driver) ? 'line-through' : ''}
+            >
+              {driver}
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
 
 const StandingChart = ({
-  driver_standings,
+  chart,
+  constructors,
 }: {
-  driver_standings: GetDriverStandingsQuery['driver_standings'];
+  chart: {
+    round: number;
+    [driverAbbreviation: string]: number;
+  }[];
+  constructors: {
+    name: string;
+    color?: string | null;
+    drivers: string[];
+  }[];
 }) => {
   const [hiddenDrivers, setHiddenDrivers] = useState<string[]>([]);
-
-  const rounds = Array.from(
-    new Set(driver_standings.map((standing) => standing.round)),
-  ).sort((a, b) => (a ?? 0) - (b ?? 0));
-
-  const drivers = Array.from(
-    new Set(driver_standings.map((standing) => standing.driver?.abbreviation)),
-  );
-
-  const chartData = rounds.map((round) => {
-    const roundData: {
-      round: number | null | undefined;
-      [key: string]: number | null | undefined;
-    } = { round };
-    drivers.forEach((driver) => {
-      if (!driver) return;
-      const standing = driver_standings.find(
-        (s) => s.round === round && s.driver?.abbreviation === driver,
-      );
-      roundData[driver] = standing ? Number(standing.points) : 0;
-    });
-    return roundData;
-  });
 
   const handleLegendClick = (driver: string) => {
     setHiddenDrivers((prev) =>
@@ -159,10 +106,10 @@ const StandingChart = ({
     <div className='container mx-auto'>
       <ResponsiveContainer width='100%' height={700}>
         <LineChart
-          data={chartData}
+          data={chart.sort((a, b) => a.round - b.round)}
           margin={{ top: 5, right: 30, left: 10, bottom: 25 }}
         >
-          <CartesianGrid strokeDasharray='3 3' />
+          <CartesianGrid strokeDasharray='3 6' />
           <Legend
             verticalAlign='top'
             // width={600}
@@ -171,10 +118,10 @@ const StandingChart = ({
               transform: 'translateX(-50%)',
               paddingBlock: '2rem',
             }}
-            content={(props) =>
+            content={() =>
               CustomLegend({
-                ...props,
-                driverStandings: driver_standings,
+                constructors: constructors,
+                hiddenDrivers: hiddenDrivers,
                 onClick: handleLegendClick,
               })
             }
@@ -182,7 +129,6 @@ const StandingChart = ({
           <XAxis dataKey='round'>
             <Label
               style={{
-                // textAnchor: 'middle',
                 fontSize: '130%',
                 fill: 'white',
               }}
@@ -199,15 +145,23 @@ const StandingChart = ({
               style={{ textAnchor: 'middle' }}
             />
           </YAxis>
-          <Tooltip />
-          {drivers.map((driver) => {
-            return (
+          <Tooltip
+            wrapperStyle={{ border: '2px red solid', width: '300px' }}
+            labelClassName='hidden'
+            contentStyle={{ fontSize: '1rem' }}
+          />
+          {constructors.map((c) => {
+            return c.drivers.map((d, i) => (
               <Line
-                key={driver}
-                hide={hiddenDrivers.includes(driver as string)}
-                {...getLineAttributes(driver, driver_standings)}
+                key={d}
+                legendType={'circle' as const}
+                dataKey={d || 'Unknown Driver'}
+                type={'monotone' as const}
+                hide={hiddenDrivers.includes(d)}
+                stroke={`#${c.color}`}
+                strokeDasharray={strokeDashArray[i]}
               />
-            );
+            ));
           })}
         </LineChart>
       </ResponsiveContainer>
@@ -226,5 +180,71 @@ export const DriverStanding = () => {
   if (error) return <p>Error loading data</p>;
   if (!data) return <p>Loading...</p>;
 
-  return <StandingChart {...data} />;
+  // Get list of drivers with constructor and color
+  const formattedData = data.driver_standings.reduce(
+    (acc, standing) => {
+      const { chart, constructors } = acc;
+      const { round } = standing; // If there's a valid driver abbreviation, assign their points
+      const driverAbbrev = standing.driver?.abbreviation;
+      const constructorName =
+        standing.driver?.driver_sessions[0].constructorByConstructorId?.name ||
+        'Unknown Driver';
+
+      // Add chart data
+      if (round && driverAbbrev) {
+        const existingRoundIndex = chart.findIndex((dp) => dp.round === round);
+        // Create round
+        if (existingRoundIndex < 0) {
+          chart.push({
+            round: round,
+            [driverAbbrev]: Number(standing.points) || 0,
+          });
+          // update round
+        } else {
+          chart[existingRoundIndex][driverAbbrev] =
+            Number(standing.points) || 0;
+        }
+      }
+
+      // Add constructor data
+      if (constructorName && driverAbbrev) {
+        const existingConstructorIndex = constructors.findIndex(
+          (c) => c.name === constructorName,
+        );
+
+        // Constructor doesn't exists start from scratch
+        if (existingConstructorIndex < 0) {
+          constructors.push({
+            name: constructorName,
+            color:
+              standing.driver?.driver_sessions[0].constructorByConstructorId
+                ?.color,
+            drivers: [driverAbbrev],
+          });
+        } else if (
+          !constructors[existingConstructorIndex].drivers.includes(driverAbbrev)
+        ) {
+          constructors[existingConstructorIndex].drivers.push(driverAbbrev);
+        }
+      }
+
+      return { chart: chart, constructors: constructors };
+    },
+    {
+      chart: [],
+      constructors: [],
+    } as {
+      chart: {
+        round: number;
+        [driverAbbreviation: string]: number;
+      }[];
+      constructors: {
+        name: string;
+        color?: string | null;
+        drivers: string[];
+      }[];
+    },
+  );
+
+  return <StandingChart {...formattedData} />;
 };
