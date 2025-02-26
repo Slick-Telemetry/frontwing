94015e526859a24dacdc058242e488062247f1a3
cris lombardo <bigc4997@gmail.com>
2025-02-26 15:46:58 -0500

    chore: FRON-58 drivers standings charting test with visx

---
+++ src/app/standings/page.tsx
@@ -0,0 +1,11 @@
+import { Standings } from './driverStandings';
+
+// Lets use this page for placeholders when not using client
+
+export default function StandingsPage() {
+  return (
+    <div className='min-h-svh'>
+      <Standings />
+    </div>
+  );
+}
