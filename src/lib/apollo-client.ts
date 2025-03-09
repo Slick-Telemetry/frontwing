df828e238ad82e61a42b16aabba9e891dda331d3
cris lombardo <bigc4997@gmail.com>
2025-03-09 15:29:04 -0400

    chore: update env params for vercel env

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -2,10 +2,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_HASURA_URL || '',
+  uri: process.env.NEXT_PUBLIC_HASURA_GRAPHQL_URL || '',
   headers: {
     'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_SECRET || '',
-    // 'skip_zrok_interstitial': 'true',
+    skip_zrok_interstitial: 'true',
   },
   cache: new InMemoryCache(),
 });
--- src/lib/client.ts
+++ src/lib/client.ts
@@ -8,10 +8,10 @@ import {
 export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   return new ApolloClient({
     link: new HttpLink({
-      uri: process.env.NEXT_PUBLIC_HASURA_URL || '',
+      uri: process.env.NEXT_PUBLIC_HASURA_GRAPHQL_URL || '',
       headers: {
         'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_SECRET || '',
-        // 'skip_zrok_interstitial': 'true',
+        skip_zrok_interstitial: 'true',
       },
     }),
     cache: new InMemoryCache(),
