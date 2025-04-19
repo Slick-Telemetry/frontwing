bc900cc0fd66f248c3c068b92f64b11a0d2630e4
Pratik Borole <borolepratik@gmail.com>
2025-04-19 10:33:30 +0530

    ci: fix typecheck error

--- src/types/static-assets.d.ts
+++ src/types/static-assets.d.ts
@@ -1,4 +1,13 @@
 declare module '*.png' {
-  const value: string;
+  import type { StaticImageData } from 'next/image';
+
+  // Create a type that behaves as StaticImageData and can be used as a string.
+  // It defines a toString() method, so when used in contexts like URL templates,
+  // the string value (`src`) is returned.
+  type ExtendedStaticImageData = StaticImageData & {
+    toString(): string;
+  };
+
+  const value: ExtendedStaticImageData;
   export default value;
 }
