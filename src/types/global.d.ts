0e90ab6094b80365f1515c989ba20ad68af4220c
cris lombardo <bigc4997@gmail.com>
2025-09-05 22:36:28 -0400

    chore(types): adjust global type location

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -4,18 +4,29 @@ import {
   GetSeasonEventsQuery,
 } from './types';
 
+// *** Require Import
 export type SeasonEvent =
   | null
   | GetSeasonEventsQuery['schedule'][0]
   | GetEventScheduleQuery['schedule'][0];
+
 export type MapEvent = GetMapEventsQuery['events'][0];
 
+// *** Helper
 interface XY {
   X: number;
   Y: number;
 }
 
-export interface CircuitDetails {
-  xy_values: XY[];
-  rotation: number;
+// *** Globally accessible, no need to import
+declare global {
+  export interface CircuitDetails {
+    xy_values: XY[];
+    rotation: number;
+  }
+  type DashParams = {
+    year: string;
+    event: string | undefined;
+    session: string | undefined;
+  };
 }
