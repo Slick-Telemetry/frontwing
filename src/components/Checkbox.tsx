1bd177c1d2e79ff76b957e2c4c8a6db32469e482
cris lombardo <bigc4997@gmail.com>
2025-09-15 23:18:40 -0400

    fix: checkbox can accept id to prevent label click overlap

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -4,14 +4,16 @@ export const CheckboxToggle = ({
   toggle,
   children,
   checked,
+  id = 'details',
 }: {
   toggle: () => void;
   children: React.ReactNode;
   checked: boolean;
+  id?: string;
 }) => {
   return (
-    <label htmlFor='details' className='flex cursor-pointer items-center gap-2'>
-      <Switch id='details' onClick={toggle} checked={checked} />
+    <label htmlFor={id} className='flex cursor-pointer items-center gap-2'>
+      <Switch id={id} onClick={toggle} checked={checked} />
       <span className='text-sm font-medium'>{children}</span>
     </label>
   );
