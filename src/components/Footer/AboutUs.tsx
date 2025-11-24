126a612f6878d27dd627142431c824b3fd933952
Pratik Borole <borolepratik@gmail.com>
2025-11-24 09:42:56 +0530

    chore(about-us): reference fastf1 and jolpica

--- src/components/Footer/AboutUs.tsx
+++ src/components/Footer/AboutUs.tsx
@@ -10,7 +10,7 @@ export const AboutUs = () => (
     <h3>Our Project</h3>
     <p>
       Slick Telemetry is a data analytics platform for Formula 1. It provides a
-      comprehensive view of the data from the cars, drivers, and teams.
+      comprehensive view of the data from the cars, drivers and teams.
     </p>
 
     <p>
@@ -18,7 +18,23 @@ export const AboutUs = () => (
       <a href='https://nextjs.org' target='_blank' rel='noreferrer'>
         Next.js
       </a>
-      , powered by{' '}
+      . Powered by{' '}
+      <a
+        href='https://github.com/theOehrly/Fast-F1'
+        target='_blank'
+        rel='noreferrer'
+      >
+        FastF1
+      </a>
+      ,{' '}
+      <a
+        href='https://github.com/jolpica/jolpica-f1'
+        target='_blank'
+        rel='noreferrer'
+      >
+        Jolpica F1
+      </a>{' '}
+      and{' '}
       <a href='https://graphql.org' target='_blank' rel='noreferrer'>
         GraphQL
       </a>
